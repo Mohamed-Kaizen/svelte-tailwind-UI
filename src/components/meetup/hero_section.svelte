8fcd8d5db5b7d705d435f191ffe7e67c5b49503d
Mohamed Nesredin <m.n.kaizen@gmail.com>
2020-07-06 20:38:57 +0300

    add hero section

---
+++ src/components/meetup/hero_section.svelte
@@ -0,0 +1,72 @@
+<div class="bg-blue-900 text-center py-8 pb-20">
+	<div class="text-white text-4xl font-bold">Find your next event</div>
+	<div class="text-gray-400">
+		<span class="mr-2">41 events in your groups</span>
+		<span class="mr-2">&middot;</span>
+		<span>3,981 events near you</span>
+	</div>
+</div>
+
+<div class="container mx-auto px-40">
+
+	<div
+		class="bg-gray-900 text-white flex items-center justify-between p-3 -my-8">
+
+		<div class="flex items-center">
+
+			<span class="relative">
+
+				<input
+					placeholder="search"
+					type="text"
+					class="text-gray-600 rounded px-2 py-3 w-64" />
+
+				<span class="absolute right-0 top-0 mr-3 mt-3">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="currentColor"
+						stroke-width="2"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						class="text-gray-500">
+						<polyline points="3 6 5 6 21 6"></polyline>
+						<path
+							d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2
+							0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
+					</svg>
+				</span>
+
+			</span>
+			<span class="ml-4">
+				within
+				<a
+					href="/"
+					class="border-b border-gray-500 hover:border-gray-100">
+					50 miles
+				</a>
+				of
+				<a
+					href="/"
+					class="border-b border-gray-500 hover:border-gray-100">
+					Tokoye, Japan
+				</a>
+			</span>
+		</div>
+		<div class="flex items-center">
+			<button class="border-l border-b border-t border-gray-700 px-6 py-2">
+				Groups
+			</button>
+			<button
+				class="border-l border-b border-t border-gray-700 bg-gray-700
+				text-white px-6 py-2 font-bold">
+				Calender
+			</button>
+		</div>
+
+	</div>
+
+</div>
