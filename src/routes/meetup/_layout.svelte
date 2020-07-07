6f2dca22c9e5ff0d1993b174caa1209779b47504
Mohamed Nesredin <m.n.kaizen@gmail.com>
2020-07-07 08:55:46 +0300

    update layout

--- src/routes/meetup/_layout.svelte
+++ src/routes/meetup/_layout.svelte
@@ -1,5 +1,12 @@
+<script>
+	import NavBar from "../../components/meetup/top_navbar.svelte"
+</script>
+
 <svelte:head>
 	<title>Meet up| svelte tailwind UI</title>
 </svelte:head>
 
-<slot />
+<div class="font-sans bg-gray-200">
+	<NavBar />
+	<slot />
+</div>
