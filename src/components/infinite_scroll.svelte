02d52d1c4c2bccfafb352ecc4f2815fc60b49d5a
Mohamed Nesredin <m.n.kaizen@gmail.com>
2020-07-07 08:34:47 +0300

    add infinite scroll

---
+++ src/components/infinite_scroll.svelte
@@ -0,0 +1,19 @@
+<script>
+	import {onMount} from "svelte"
+	export let callball
+	let inf_scroll
+	if (typeof window !== "undefined") {
+		onMount(() => {
+			new IntersectionObserver((enteries) => {
+				enteries.forEach((entery) => {
+					if (entery.intersectionRatio > 0) {
+						callball()
+					}
+				})
+			}).observe(inf_scroll)
+		})
+	}
+</script>
+
+<slot name="content" />
+<div bind:this="{inf_scroll}"></div>
