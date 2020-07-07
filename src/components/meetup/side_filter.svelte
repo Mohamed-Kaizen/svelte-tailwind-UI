9869c2ad77c96caae1d3737678bca806ccdc25f7
Mohamed Nesredin <m.n.kaizen@gmail.com>
2020-07-07 09:30:51 +0300

    add side filter

---
+++ src/components/meetup/side_filter.svelte
@@ -0,0 +1,20 @@
+<div class="border border-gray-400 bg-white text-sm ">
+
+	<div class="border-b border-gray-400 flex px-4 py-2">
+		<a href="/">All My events</a>
+	</div>
+
+	<div
+		class="border-b border-gray-400 flex font-bold bg-purple-100
+		text-indigo-500">
+
+		<div class="border-l-4 border-indigo-500 pl-3 py-2">
+			<a href="/">All Upcoming events</a>
+		</div>
+
+	</div>
+
+	<div class="border-b border-gray-400 flex px-4 py-2">
+		<a href="/">All Old events</a>
+	</div>
+</div>
