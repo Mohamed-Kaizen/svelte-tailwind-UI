b8e68a0b45ac63eab64f1083931897713e609338
Mohamed Nesredin <m.n.kaizen@gmail.com>
2020-07-07 08:55:35 +0300

    add cards

---
+++ src/components/meetup/card_event.svelte
@@ -0,0 +1,27 @@
+<script>
+	export let indexo
+</script>
+
+<div class="border-b border-gray-400 flex p-4">
+
+	<div class="w-16 mr-4 text-gray-600">
+		<a href="/">{indexo + 6}:00PM</a>
+	</div>
+
+	<div>
+		<div class="uppercase font-bold text-gray-700 text-sm">
+			<a href="/">Japan python users group</a>
+		</div>
+
+		<div class="font-bold text-gray-800 text-xl">
+			<a href="/">Pycon 2021</a>
+		</div>
+
+		<div class="text-gray-600 flex">
+			<span class="mr-2">31 python coder going</span>
+			<span class="text-red-400 mr-2">&middot;</span>
+			<span class="text-red-400">{indexo * 2} spots left</span>
+		</div>
+	</div>
+
+</div>
---
+++ src/components/meetup/simple_card.svelte
@@ -0,0 +1,13 @@
+<div class="w-1/2 mr-2">
+	<div class="bg-white shadow mb-4">
+		<div class="px-6 py-4">
+			<div class="font-bold">
+				<a href="/">Usability</a>
+			</div>
+			<div class="text-gray-700 mb-1">593 Follow this topic</div>
+			<div>
+				<a href="/" class="text-teal-600 hover:underline">Host events</a>
+			</div>
+		</div>
+	</div>
+</div>
