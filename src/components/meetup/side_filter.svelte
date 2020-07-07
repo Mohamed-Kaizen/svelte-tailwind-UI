e698cf14a2e44c2628b9772ddaaabaa8d3948766
Mohamed Nesredin <m.n.kaizen@gmail.com>
2020-07-07 11:26:54 +0300

    add dark mode to side filter

--- src/components/meetup/side_filter.svelte
+++ src/components/meetup/side_filter.svelte
@@ -1,20 +1,23 @@
-<div class="border border-gray-400 bg-white text-sm ">
+<div
+	class="border border-gray-400 dark:border-transparent bg-white
+	dark:bg-gray-900 text-sm dark:text-gray-400">
 
-	<div class="border-b border-gray-400 flex px-4 py-2">
+	<div class="flex px-4 py-2">
 		<a href="/">All My events</a>
 	</div>
 
 	<div
-		class="border-b border-gray-400 flex font-bold bg-purple-100
-		text-indigo-500">
+		class="flex font-bold bg-purple-100 text-indigo-500 dark:bg-gray-400
+		dark:text-indigo-700">
 
-		<div class="border-l-4 border-indigo-500 pl-3 py-2">
+		<div
+			class="border-l-4 border-indigo-500 dark:border-indigo-700 pl-3 py-2">
 			<a href="/">All Upcoming events</a>
 		</div>
 
 	</div>
 
-	<div class="border-b border-gray-400 flex px-4 py-2">
+	<div class="flex px-4 py-2">
 		<a href="/">All Old events</a>
 	</div>
 </div>
