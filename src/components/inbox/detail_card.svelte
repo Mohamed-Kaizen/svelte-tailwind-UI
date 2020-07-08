cacdadac40720ab303ade4a1f4c3c8d756630c80
Mohamed Nesredin <m.n.kaizen@gmail.com>
2020-07-08 15:05:52 +0300

    add inbox detail card, toolbar

---
+++ src/components/inbox/detail_card.svelte
@@ -0,0 +1,22 @@
+<script>
+	export let username, message_type, date, image, message
+</script>
+
+<article class="mb-3 px-10 pt-6 pb-8 bg-white rounded-lg shadow">
+	<div class="flex items-center justify-between">
+		<p class="text-lg font-semibold">
+			<span class="text-gray-900">{username}</span>
+			<span class="text-gray-600">{message_type}</span>
+		</p>
+		<div class="flex items-center">
+			<span class="text-sm text-gray-700">{date}</span>
+			<img
+				class="ml-5 h-10 w-10 rounded-full object-cover"
+				src="{image}"
+				alt="madara profile" />
+		</div>
+	</div>
+	<div class="mt-6 text-sm text-gray-800">
+		<p>{message}</p>
+	</div>
+</article>
---
+++ src/components/inbox/message_toolbar.svelte
@@ -0,0 +1,108 @@
+<div class="flex items-center justify-between px-5 py-4 bg-gray-100 border-b">
+
+	<div class="flex items-center">
+
+		<button
+			class="focus:outline-none p-2 text-center focus:bg-gray-300
+			hover:bg-gray-300 rounded-full">
+			<svg class="h-6 w-6 fill-current text-gray-600" viewBox="0 0 24 24">
+				<path
+					d="M8 9.8v.9l1.7.3c2.6.4 4.5 1.4 5.9
+					2.7-1.7-.5-3.5-.8-5.6-.8H8v1.3L5.8 12 8 9.8M10 5l-7 7 7
+					7v-4.1c5 0 8.5 1.6 11 5.1-1-5-4-10-11-11"></path>
+			</svg>
+		</button>
+
+		<button
+			class="focus:outline-none p-2 text-center focus:bg-gray-300
+			hover:bg-gray-300 rounded-full">
+			<svg class="h-6 w-6 fill-current text-gray-600" viewBox="0 0 24 24">
+				<path
+					d="M9.47 9.65l-1.41 1.42L11 14l5.19-5.18-1.41-1.42L11
+					11.18M17 3H7c-1.1 0-2 .9-2 2v16l7-3 7 3V5c0-1.1-.9-2-2-2m0
+					15l-5-2.18L7 18V5h10z"></path>
+			</svg>
+		</button>
+
+		<button
+			class="focus:outline-none p-2 text-center focus:bg-gray-300
+			hover:bg-gray-300 rounded-full">
+			<svg
+				class="h-6 w-6 fill-current text-gray-600"
+				viewBox="0 0 512 512">
+				<path
+					d="M451.4 369.1C468.7 356 480 335.4 480
+					312c0-39.8-32.2-72-72-72h-14.1c13.4-11.7 22.1-28.8 22.1-48
+					0-35.3-28.7-64-64-64h-5.9c3.6-10.1 5.9-20.7 5.9-32
+					0-53-43-96-96-96-5.2 0-10.2.7-15.1 1.5C250.3 14.6 256 30.6
+					256 48c0 44.2-35.8 80-80 80h-16c-35.3 0-64 28.7-64 64 0 19.2
+					8.7 36.3 22.1 48H104c-39.8 0-72 32.2-72 72 0 23.4 11.3 44
+					28.6 57.1C26.3 374.6 0 404.1 0 440c0 39.8 32.2 72 72
+					72h368c39.8 0 72-32.2 72-72 0-35.9-26.3-65.4-60.6-70.9zM192
+					256c17.7 0 32 14.3 32 32s-14.3 32-32 32-32-14.3-32-32 14.3-32
+					32-32zm159.5 139C341 422.9 293 448 256
+					448s-85-25.1-95.5-53c-2-5.3 2-11 7.8-11h175.4c5.8 0 9.8 5.7
+					7.8 11zM320 320c-17.7 0-32-14.3-32-32s14.3-32 32-32 32 14.3
+					32 32-14.3 32-32 32z"></path>
+			</svg>
+		</button>
+
+		<button
+			class="focus:outline-none p-2 text-center focus:bg-gray-300
+			hover:bg-gray-300 rounded-full">
+			<svg class="h-6 w-6 fill-current text-gray-600" viewBox="0 0 24 24">
+				<path
+					d="M8 17v-2h8v2H8m8-7l-4 4-4-4h2.5V6h3v4H16m-4-8c5.5 0 10 4.5
+					10 10s-4.5 10-10 10S2 17.5 2 12 6.5 2 12 2m0 2c-4.42 0-8
+					3.58-8 8s3.58 8 8 8 8-3.58 8-8-3.58-8-8-8z"></path>
+			</svg>
+		</button>
+
+		<button
+			class="focus:outline-none p-2 text-center focus:bg-gray-300
+			hover:bg-gray-300 rounded-full">
+			<svg
+				class="h-6 w-6 fill-current text-gray-600"
+				viewBox="0 0 512 512">
+				<path
+					d="M328 256c0 39.8-32.2 72-72 72s-72-32.2-72-72 32.2-72 72-72
+					72 32.2 72 72zm104-72c-39.8 0-72 32.2-72 72s32.2 72 72 72
+					72-32.2 72-72-32.2-72-72-72zm-352 0c-39.8 0-72 32.2-72
+					72s32.2 72 72 72 72-32.2 72-72-32.2-72-72-72z"></path>
+			</svg>
+		</button>
+	</div>
+
+	<div class="flex items-center">
+		<button
+			class="focus:outline-none p-2 text-center focus:bg-gray-300
+			hover:bg-gray-300 rounded-full">
+			<svg
+				class="h-6 w-6 fill-current text-gray-600"
+				viewBox="0 0 448 512">
+				<path
+					d="M240.971 130.524l194.343 194.343c9.373 9.373 9.373 24.569
+					0 33.941l-22.667 22.667c-9.357 9.357-24.522
+					9.375-33.901.04L224 227.495 69.255 381.516c-9.379
+					9.335-24.544
+					9.317-33.901-.04l-22.667-22.667c-9.373-9.373-9.373-24.569
+					0-33.941L207.03 130.525c9.372-9.373 24.568-9.373 33.941-.001z"></path>
+			</svg>
+		</button>
+		<button
+			class="focus:outline-none p-2 text-center focus:bg-gray-300
+			hover:bg-gray-300 rounded-full">
+			<svg
+				class="h-6 w-6 fill-current text-gray-600"
+				viewBox="0 0 448 512">
+				<path
+					d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569
+					0-33.941l22.667-22.667c9.357-9.357 24.522-9.375
+					33.901-.04L224 284.505l154.745-154.021c9.379-9.335
+					24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569
+					0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z"></path>
+			</svg>
+		</button>
+	</div>
+
+</div>
