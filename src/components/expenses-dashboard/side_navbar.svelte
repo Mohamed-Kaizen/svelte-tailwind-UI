dade56a8fc21c45b97a166a7d4a8e47f5c834f82
Mohamed Nesredin <m.n.kaizen@gmail.com>
2020-07-08 20:15:14 +0300

    add expenses-dashboard side navbar

---
+++ src/components/expenses-dashboard/side_navbar.svelte
@@ -0,0 +1,179 @@
+<nav class="w-56 bg-white select-none overflow-y-auto">
+
+	<div class="flex flex-col items-center ">
+		<h1 class="text-6xl font-light text-pink-600 mt-0">pleo</h1>
+
+		<img
+			class="h-16 w-16 rounded-full object-cover mt-4"
+			src="/mickey_mouse.jpg"
+			alt="mickey mouse Profile" />
+		<span class="capitalize mt-2 mb-6">mickey</span>
+	</div>
+
+	<ul>
+		<li class="pl-8 py-2 font-semibold text-gray-700 hover:bg-pink-200 mb-2">
+
+			<button class="focus:text-pink-500 focus:outline-none w-full">
+
+				<span class="flex items-center">
+					<svg class="h-4 w-4 fill-current" viewBox="0 0 576 512">
+						<path
+							d="M527.9 32H48.1C21.5 32 0 53.5 0 80v352c0 26.5 21.5
+							48 48.1 48h479.8c26.6 0 48.1-21.5
+							48.1-48V80c0-26.5-21.5-48-48.1-48zM54.1 80h467.8c3.3
+							0 6 2.7 6 6v42H48.1V86c0-3.3 2.7-6 6-6zm467.8
+							352H54.1c-3.3 0-6-2.7-6-6V256h479.8v170c0 3.3-2.7 6-6
+							6zM192 332v40c0 6.6-5.4 12-12 12h-72c-6.6
+							0-12-5.4-12-12v-40c0-6.6 5.4-12 12-12h72c6.6 0 12 5.4
+							12 12zm192 0v40c0 6.6-5.4 12-12 12H236c-6.6
+							0-12-5.4-12-12v-40c0-6.6 5.4-12 12-12h136c6.6 0 12
+							5.4 12 12z"></path>
+					</svg>
+					<span class="ml-4 capitalize">my card</span>
+				</span>
+			</button>
+		</li>
+
+		<li
+			class="pl-8 py-2 font-semibold text-pink-500 border-l-2
+			border-pink-500 hover:bg-pink-200 mb-2">
+
+			<button class="focus:text-pink-500 focus:outline-none w-full">
+
+				<span class="flex items-center">
+					<svg class="h-4 w-4 fill-current" viewBox="0 0 24 24">
+						<path
+							d="M15 5v4h-4v4H7v4H3v3h7v-4h4v-4h4V8h4V5h-7z"></path>
+					</svg>
+
+					<span class="ml-4 capitalize">expenses</span>
+				</span>
+			</button>
+		</li>
+
+		<li class="pl-8 py-2 font-semibold text-gray-700 hover:bg-pink-200 mb-2">
+
+			<button class="focus:text-pink-500 focus:outline-none w-full">
+
+				<span class="flex items-center">
+					<svg class="h-4 w-4 fill-current" viewBox="0 0 24 24">
+						<path
+							d="M20 10H4V4h16m0 11H4v-2h16m0-11H4c-1.11 0-2 .89-2
+							2v11c0 1.11.89 2 2 2h4v5l4-2 4 2v-5h4c1.11 0 2-.89
+							2-2V4c0-1.11-.89-2-2-2z"></path>
+					</svg>
+					<span class="ml-4 capitalize">wallet</span>
+				</span>
+			</button>
+		</li>
+
+		<li class="pl-8 py-2 font-semibold text-gray-700 hover:bg-pink-200 mb-2">
+
+			<button class="focus:text-pink-500 focus:outline-none w-full">
+
+				<span class="flex items-center">
+					<svg class="h-4 w-4 fill-current" viewBox="0 0 24 24">
+						<path
+							d="M16 9c6 0 6 4 6
+							4v2h-6v-2s0-1.69-1.15-3.2c-.17-.23-.38-.45-.6-.66C14.77
+							9.06 15.34 9 16 9m-8 2c3.5 0 3.94 1.56 4
+							2H4c.06-.44.5-2 4-2m0-2c-6 0-6 4-6
+							4v2h12v-2s0-4-6-4m1 8v2h6v-2l3 3-3 3v-2H9v2l-3-3
+							3-3M8 3c.55 0 1 .45 1 1s-.45 1-1 1-1-.45-1-1 .45-1
+							1-1m0-2C6.34 1 5 2.34 5 4s1.34 3 3 3 3-1.34
+							3-3-1.34-3-3-3m8 0c-1.66 0-3 1.34-3 3s1.34 3 3 3
+							3-1.34 3-3-1.34-3-3-3z"></path>
+					</svg>
+					<span class="ml-4 capitalize">manage team</span>
+				</span>
+			</button>
+		</li>
+
+		<li class="pl-8 py-2 font-semibold text-gray-700 hover:bg-pink-200 mb-2">
+
+			<button class="focus:text-pink-500 focus:outline-none w-full">
+
+				<span class="flex items-center">
+					<svg class="h-4 w-4 fill-current" viewBox="0 0 24 24">
+						<path
+							d="M21 8V6H7v2h14m0 8v-5H7v5h14m0-12a2 2 0 012 2v10a2
+							2 0 01-2 2H7a2 2 0 01-2-2V6c0-1.11.89-2 2-2h14M3
+							20h15v2H3a2 2 0 01-2-2V9h2v11z"></path>
+					</svg>
+					<span class="ml-4 capitalize">manage cards</span>
+				</span>
+			</button>
+		</li>
+
+		<li class="pl-8 py-2 font-semibold text-gray-700 hover:bg-pink-200 mb-2">
+
+			<button class="focus:text-pink-500 focus:outline-none w-full">
+
+				<span class="flex items-center">
+					<svg class="h-4 w-4 fill-current" viewBox="0 0 448 512">
+						<path
+							d="M128 148v-40c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12
+							12v40c0 6.6-5.4 12-12 12h-40c-6.6 0-12-5.4-12-12zm140
+							12h40c6.6 0 12-5.4
+							12-12v-40c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12
+							12v40c0 6.6 5.4 12 12 12zm-128 96h40c6.6 0 12-5.4
+							12-12v-40c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12
+							12v40c0 6.6 5.4 12 12 12zm128 0h40c6.6 0 12-5.4
+							12-12v-40c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12
+							12v40c0 6.6 5.4 12 12 12zm-76
+							84v-40c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12
+							12v40c0 6.6 5.4 12 12 12h40c6.6 0 12-5.4 12-12zm76
+							12h40c6.6 0 12-5.4
+							12-12v-40c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12
+							12v40c0 6.6 5.4 12 12 12zm180 124v36H0v-36c0-6.6
+							5.4-12 12-12h19.5V24c0-13.3 10.7-24 24-24h337c13.3 0
+							24 10.7 24 24v440H436c6.6 0 12 5.4 12 12zM79.5
+							463H192v-67c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12
+							12v67h112.5V49L80 48l-.5 415z"></path>
+					</svg>
+					<span class="ml-4 capitalize">company profile</span>
+				</span>
+			</button>
+		</li>
+
+		<li class="pl-8 py-2 font-semibold text-gray-700 hover:bg-pink-200 mb-2">
+
+			<button class="focus:text-pink-500 focus:outline-none w-full">
+
+				<span class="flex items-center">
+					<svg class="h-4 w-4 fill-current" viewBox="0 0 24 24">
+						<path d="M3 13h12v-2H3m0-5v2h18V6M3 18h6v-2H3v2z"></path>
+					</svg>
+					<span class="ml-4 capitalize">manage categories</span>
+				</span>
+			</button>
+		</li>
+
+		<li class="pl-8 py-2 font-semibold text-gray-700 hover:bg-pink-200 mb-2">
+
+			<button class="focus:text-pink-500 focus:outline-none w-full">
+
+				<span class="flex items-center">
+					<svg class="h-4 w-4 fill-current" viewBox="0 0 496 512">
+						<path
+							d="M248 104c-53 0-96 43-96 96s43 96 96 96 96-43
+							96-96-43-96-96-96zm0 144c-26.5
+							0-48-21.5-48-48s21.5-48 48-48 48 21.5 48 48-21.5
+							48-48 48zm0-240C111 8 0 119 0 256s111 248 248 248
+							248-111 248-248S385 8 248 8zm0 448c-49.7
+							0-95.1-18.3-130.1-48.4 14.9-23 40.4-38.6 69.6-39.5
+							20.8 6.4 40.6 9.6 60.5 9.6s39.7-3.1 60.5-9.6c29.2 1
+							54.7 16.5 69.6 39.5-35 30.1-80.4 48.4-130.1
+							48.4zm162.7-84.1c-24.4-31.4-62.1-51.9-105.1-51.9-10.2
+							0-26 9.6-57.6 9.6-31.5 0-47.4-9.6-57.6-9.6-42.9
+							0-80.6 20.5-105.1 51.9C61.9 339.2 48 299.2 48
+							256c0-110.3 89.7-200 200-200s200 89.7 200 200c0
+							43.2-13.9 83.2-37.3 115.9z"></path>
+					</svg>
+					<span class="ml-4 capitalize">my profile</span>
+				</span>
+			</button>
+		</li>
+
+	</ul>
+</nav>
