d4b7b290699cf7435342cdc9e031b2f90afe806a
Mohamed Nesredin <m.n.kaizen@gmail.com>
2020-07-09 19:07:15 +0300

    add header list for user dashboard

---
+++ src/components/users-dashboard/header.svelte
@@ -0,0 +1,41 @@
+<h2 class="my-4 text-4xl font-semibold">User list</h2>
+<div
+	class="pb-2 flex items-center justify-between text-gray-600
+	dark:text-gray-400 border-b dark:border-gray-600">
+	<div>
+		<span>
+			<span class="text-green-500 dark:text-green-200">431</span>
+			users;
+		</span>
+		<span>
+			<span class="text-green-500 dark:text-green-200">22</span>
+			projects;
+		</span>
+		<span>
+			<span class="text-green-500 dark:text-green-200">33</span>
+			roles
+		</span>
+	</div>
+	<div>
+		<span class="capitalize">
+			project
+			<span class="text-green-500 dark:text-green-200 cursor-pointer">
+				all
+			</span>
+		</span>
+		<span class="capitalize ml-12">
+			date added
+			<span class="text-green-500 dark:text-green-200 cursor-pointer">
+				all
+			</span>
+		</span>
+		<span class="capitalize ml-12">
+			role
+			<span class="text-green-500 dark:text-green-200 cursor-pointer">
+				all
+			</span>
+		</span>
+
+	</div>
+
+</div>
