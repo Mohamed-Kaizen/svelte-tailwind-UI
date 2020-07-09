3684f7aef67986731e1073730e41b8125611fac1
Mohamed Nesredin <m.n.kaizen@gmail.com>
2020-07-09 13:26:56 +0300

    fix the home page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,18 @@
-<a href="/meetup/">Meetup</a>
-<a href="/inbox/">Inbox</a>
+<div class="container mx-auto p-8 flex flex-row flex-wrap justify-between">
+	<div class="m-8 shadow px-6 py-2 text-blue-500 bg-white">
+		<a href="/meetup/">Meetup</a>
+
+	</div>
+
+	<div class="m-8 shadow px-6 py-2 bg-white">
+		<a class="text-blue-500" href="/inbox/">Inbox</a>
+
+	</div>
+
+	<div class="m-8 shadow px-6 py-2 flex flex-col text-blue-500 bg-white">
+		<a href="/expenses-dashboard/">Expenses Dashboard</a>
+		<a class="mt-3" href="expense.png/">Base Desgin</a>
+
+	</div>
+
+</div>
