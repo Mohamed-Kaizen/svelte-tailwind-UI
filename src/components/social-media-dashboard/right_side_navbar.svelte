7793f685d5f066ed26ec5f64f331ab13692f52f6
Mohamed Nesredin <m.n.kaizen@gmail.com>
2020-07-10 15:40:32 +0300

    add right side navbar for social media dashboard

---
+++ src/components/social-media-dashboard/right_side_navbar.svelte
@@ -0,0 +1,93 @@
+<aside class="w-1/4 px-12 py-4 flex flex-col bg-white">
+
+	<div class="flex items-center justify-between">
+		<!-- Actions -->
+
+		<a href="inbox/">
+			<svg
+				class="h-5 w-5 hover:text-red-600"
+				viewBox="0 0 24 24"
+				fill="none"
+				stroke="currentColor"
+				stroke-width="2"
+				stroke-linecap="round"
+				stroke-linejoin="round">
+				<path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path>
+				<path d="M13.73 21a2 2 0 0 1-3.46 0"></path>
+			</svg>
+		</a>
+
+		<button
+			class="flex items-center py-2 px-3 text-red-600 bg-red-200 rounded-lg
+			uppercase">
+
+			<svg class="h-5 w-5 fill-current mr-1" viewBox="0 0 24 24">
+				<path
+					d="M9.06 1.93C7.17 1.92 5.33 3.74 6.17 6H3a2 2 0 00-2 2v2a1 1
+					0 001 1h9V8h2v3h9a1 1 0 001-1V8a2 2 0 00-2-2h-3.17C19 2.73
+					14.6.42 12.57 3.24L12
+					4l-.57-.78c-.63-.89-1.5-1.28-2.37-1.29M9 4c.89 0 1.34 1.08.71
+					1.71C9.08 6.34 8 5.89 8 5a1 1 0 011-1m6 0c.89 0 1.34 1.08.71
+					1.71-.63.63-1.71.18-1.71-.71a1 1 0 011-1M2 12v8a2 2 0 002
+					2h16a2 2 0 002-2v-8h-9v8h-2v-8H2z"></path>
+			</svg>
+			<span>2 new updates</span>
+		</button>
+
+		<a href="users-dashboard/">
+			<svg
+				class="h-5 w-5 fill-current hover:text-red-600"
+				viewBox="0 0 448 512">
+				<path
+					d="M313.6 304c-28.7 0-42.5 16-89.6 16-47.1
+					0-60.8-16-89.6-16C60.2 304 0 364.2 0 438.4V464c0 26.5 21.5 48
+					48 48h352c26.5 0 48-21.5
+					48-48v-25.6c0-74.2-60.2-134.4-134.4-134.4zM400
+					464H48v-25.6c0-47.6 38.8-86.4 86.4-86.4 14.6 0 38.3 16 89.6
+					16 51.7 0 74.9-16 89.6-16 47.6 0 86.4 38.8 86.4 86.4V464zM224
+					288c79.5 0 144-64.5 144-144S303.5 0 224 0 80 64.5 80 144s64.5
+					144 144 144zm0-240c52.9 0 96 43.1 96 96s-43.1 96-96
+					96-96-43.1-96-96 43.1-96 96-96z"></path>
+			</svg>
+		</a>
+	</div>
+
+	<img class="mt-8" src="social_media_dashboard_draw.png/" alt="team spirit" />
+
+	<img class="mt-4" src="social_media_dashboard_calender.png" alt="" />
+
+	<div class="mt-4 py-3 flex items-center justify-between border rounded-lg">
+		<!-- Card -->
+
+		<span></span>
+
+		<span class="text-sm">
+			Say Hi to
+			<strong>Joker</strong>
+		</span>
+
+		<img
+			class="mr-4 h-10 w-10 rounded-full object-cover"
+			src="joker.jpg/"
+			alt="joker profile" />
+
+	</div>
+
+	<div class="mt-4 py-3 flex items-center justify-between border rounded-lg">
+		<!-- Card -->
+
+		<span></span>
+
+		<span class="text-sm">
+			Say Hi to
+			<strong>Ghost Rider</strong>
+		</span>
+
+		<img
+			class="mr-4 h-10 w-10 rounded-full object-cover"
+			src="ghost_rider.jpg/"
+			alt="Ghost Rider profile" />
+
+	</div>
+
+</aside>
