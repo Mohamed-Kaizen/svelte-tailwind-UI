db2fed8edc09851d547cb3c1b6766f66fd33bc7e
Mohamed Nesredin <m.n.kaizen@gmail.com>
2020-07-10 09:12:36 +0300

    add user dashboard page

---
+++ src/routes/users-dashboard/_layout.svelte
@@ -0,0 +1,5 @@
+<svelte:head>
+	<title>Users DashBoard | svelte tailwind UI</title>
+</svelte:head>
+
+<slot />
---
+++ src/routes/users-dashboard/index.svelte
@@ -0,0 +1,30 @@
+<script>
+	import SideNavBar from "../../components/users-dashboard/side_navbar.svelte"
+	import TopNavBar from "../../components/users-dashboard/top_navbar.svelte"
+	import Header from "../../components/users-dashboard/header.svelte"
+	import ListHeader from "../../components/users-dashboard/list_header.svelte"
+	import Card from "../../components/users-dashboard/card.svelte"
+</script>
+
+<div class="h-screen w-full flex overflow-hidden">
+	<SideNavBar />
+
+	<div
+		class="flex-1 flex flex-col bg-gray-100 dark:bg-gray-700 transition
+		duration-500 ease-in-out overflow-y-auto">
+
+		<div class="mx-10 my-2">
+
+			<TopNavBar />
+
+			<Header />
+
+			<ListHeader />
+
+			<Card />
+
+		</div>
+
+	</div>
+
+</div>
