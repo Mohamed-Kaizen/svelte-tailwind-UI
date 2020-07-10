7f7e8de2cc0e813a0aabef4245e75b99a1d8e49b
Mohamed Nesredin <m.n.kaizen@gmail.com>
2020-07-10 09:05:59 +0300

    fix text in dark moce

--- src/components/users-dashboard/header.svelte
+++ src/components/users-dashboard/header.svelte
@@ -1,4 +1,4 @@
-<h2 class="my-4 text-4xl font-semibold">User list</h2>
+<h2 class="my-4 text-4xl font-semibold dark:text-gray-400">User list</h2>
 <div
 	class="pb-2 flex items-center justify-between text-gray-600
 	dark:text-gray-400 border-b dark:border-gray-600">
@@ -26,7 +26,7 @@
 		<span class="capitalize ml-12">
 			date added
 			<span class="text-green-500 dark:text-green-200 cursor-pointer">
-				all
+				all time
 			</span>
 		</span>
 		<span class="capitalize ml-12">
