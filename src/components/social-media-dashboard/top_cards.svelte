7fcfc9393ccc60cb2b2653a207d6467552b4fcdf
Mohamed Nesredin <m.n.kaizen@gmail.com>
2020-07-10 20:16:40 +0300

    add top cards

---
+++ src/components/social-media-dashboard/top_cards.svelte
@@ -0,0 +1,154 @@
+<div class="mt-8 flex items-center justify-between">
+
+	<div
+		class="px-4 pt-4 pb-2 flex justify-between border-4 border-white
+		rounded-lg">
+		<!-- Card -->
+
+		<div class="flex flex-col">
+			<!-- Left section -->
+
+			<svg
+				class="h-6 w-6 fill-current text-blue-500"
+				viewBox="0 0 512 512">
+				<path
+					d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0
+					138.72-105.583 298.558-298.558 298.558-59.452
+					0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34
+					1.299 49.055 0 94.213-16.568
+					130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772
+					6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3
+					27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969
+					7.797 30.214 12.67 47.431
+					13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492
+					5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365
+					109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828
+					46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67
+					33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798
+					24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122
+					60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"></path>
+			</svg>
+			<span class="mt-4 text-4xl font-semibold">280k</span>
+			<span class="text-sm text-gray-600">followers</span>
+			<span class="text-transparent">&sbquo</span>
+		</div>
+
+		<div class="ml-8 flex flex-col justify-between">
+			<!-- Right section -->
+
+			<span></span>
+			<span class="bottom-0">..</span>
+		</div>
+
+	</div>
+
+	<div
+		class="px-4 pt-4 pb-2 flex justify-between border-4 border-white
+		rounded-lg bg-white">
+		<!-- Card -->
+
+		<div class="flex flex-col">
+			<!-- Left section -->
+
+			<svg
+				class="h-6 w-6 fill-current text-purple-500"
+				viewBox="0 0 448 512">
+				<path
+					d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9
+					114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1
+					0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7
+					74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8
+					26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8
+					26.8zm76.1
+					27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9
+					0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1
+					147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9
+					36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2
+					26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8
+					388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1
+					9s-102.7
+					2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7
+					9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9
+					132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7
+					29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"></path>
+			</svg>
+			<span class="mt-4 text-4xl font-semibold">680k</span>
+			<span class="text-sm text-gray-600">followers</span>
+			<span class="text-transparent">&sbquo</span>
+		</div>
+
+		<div class="ml-8 flex flex-col justify-between">
+			<!-- Right section -->
+
+			<svg class="h-5 w-5 fill-current" viewBox="0 0 24 24">
+				<path
+					d="M3.5 18.5l6-6 4 4L22 6.92 20.59 5.5l-7.09 8-4-4L2 17l1.5
+					1.5z"></path>
+			</svg>
+
+			<span class="bottom-0">..</span>
+		</div>
+
+	</div>
+	<div
+		class="px-4 pt-4 pb-2 flex justify-between border-4 border-white
+		rounded-lg">
+		<!-- Card -->
+
+		<div class="flex flex-col">
+			<!-- Left section -->
+			<svg class="h-6 w-6 fill-current text-red-600" viewBox="0 0 24 24">
+				<path
+					d="M10 15l5.19-3L10 9v6m11.56-7.83c.13.47.22 1.1.28
+					1.9.07.8.1 1.49.1 2.09L22 12c0 2.19-.16 3.8-.44
+					4.83-.25.9-.83 1.48-1.73
+					1.73-.47.13-1.33.22-2.65.28-1.3.07-2.49.1-3.59.1L12 19c-4.19
+					0-6.8-.16-7.83-.44-.9-.25-1.48-.83-1.73-1.73-.13-.47-.22-1.1-.28-1.9-.07-.8-.1-1.49-.1-2.09L2
+					12c0-2.19.16-3.8.44-4.83.25-.9.83-1.48 1.73-1.73.47-.13
+					1.33-.22 2.65-.28 1.3-.07 2.49-.1 3.59-.1L12 5c4.19 0 6.8.16
+					7.83.44.9.25 1.48.83 1.73 1.73z"></path>
+			</svg>
+			<span class="mt-4 text-4xl font-semibold">2.3M</span>
+			<span class="text-sm text-gray-600">followers</span>
+			<span class="text-transparent">&sbquo</span>
+		</div>
+
+		<div class="ml-8 flex flex-col justify-between">
+			<!-- Right section -->
+
+			<span></span>
+			<span class="bottom-0">..</span>
+		</div>
+
+	</div>
+
+	<div
+		class="px-4 py-4 pb-2 flex justify-between border-4 border-white
+		rounded-lg">
+		<!-- Card -->
+
+		<div class="flex flex-col">
+			<!-- Left section -->
+			<svg class="h-6 w-6 fill-current text-blue-500" viewBox="0 0 24 24">
+				<path
+					d="M12 2.04c-5.5 0-10 4.49-10 10.02 0 5 3.66 9.15 8.44
+					9.9v-7H7.9v-2.9h2.54V9.85c0-2.51 1.49-3.89 3.78-3.89 1.09 0
+					2.23.19 2.23.19v2.47h-1.26c-1.24 0-1.63.77-1.63
+					1.56v1.88h2.78l-.45 2.9h-2.33v7a10 10 0
+					008.44-9.9c0-5.53-4.5-10.02-10-10.02z"></path>
+			</svg>
+			<span class="mt-4 text-4xl font-semibold">280k</span>
+			<span class="text-sm text-gray-600">followers</span>
+			<span class="text-transparent">&sbquo</span>
+		</div>
+
+		<div class="ml-8 flex flex-col justify-between">
+			<!-- Right section -->
+
+			<span></span>
+			<span class="bottom-0">..</span>
+		</div>
+
+	</div>
+
+</div>
