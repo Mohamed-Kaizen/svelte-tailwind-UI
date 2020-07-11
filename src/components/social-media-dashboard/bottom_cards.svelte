94473c1d135370ead66328825761ca982090fa9d
Mohamed Nesredin <m.n.kaizen@gmail.com>
2020-07-11 07:31:31 +0300

    add bottom cards

---
+++ src/components/social-media-dashboard/bottom_cards.svelte
@@ -0,0 +1,116 @@
+<div class="mt-6 flex justify-between capitalize">
+	<!-- Bottom cards -->
+
+	<div class="p-3 bg-white rounded-lg">
+		<!-- info's -->
+
+		<h3 class="text-xl font-semibold">key metricks</h3>
+
+		<div class="mt-4">
+
+			<div class="pb-2 mb-4 flex justify-between border-b ">
+
+				<span>clicks</span>
+
+				<div class="flex items-center">
+					<span>10k</span>
+					<input type="range" class="mx-3" />
+					<span>12k</span>
+
+				</div>
+
+			</div>
+
+			<div class="pb-2 mb-4 flex justify-between border-b ">
+
+				<span>liks</span>
+
+				<div class="flex items-center ml-24">
+					<span>680</span>
+					<input type="range" class="mx-3" />
+					<span>520</span>
+
+				</div>
+
+			</div>
+
+			<div class="pb-2 mb-4 flex justify-between border-b ">
+
+				<span>followers</span>
+
+				<div class="flex items-center">
+					<span>8k</span>
+					<input type="range" class="mx-3" />
+					<span>12.5k</span>
+
+				</div>
+
+			</div>
+
+			<div class="flex justify-between">
+
+				<span>impressions</span>
+
+				<div class="flex items-center">
+					<span>369</span>
+					<input type="range" class="mx-3" />
+					<span>10k</span>
+
+				</div>
+
+			</div>
+
+		</div>
+
+	</div>
+
+	<div
+		class="px-10 py-4 pb-2 mx-4 flex flex-col items-center justify-between
+		text-white bg-pink-900 rounded-lg capitalize text-center">
+		<!-- Card -->
+
+		<div>
+			<h2>engaged users</h2>
+			<h1 class="mt-2 text-4xl font-semibold">12.2k</h1>
+		</div>
+
+		<div class="flex flex-row items-center">
+			<svg
+				class="h-5 w-5 fill-current text-green-400 mr-3"
+				viewBox="0 0 24 24">
+				<path
+					d="M16 13v8H8v-8H2L12 3l10 10h-6m-9-2h3v8h4v-8h3l-5-5-5 5z"></path>
+			</svg>
+
+			<span>19%</span>
+
+		</div>
+
+	</div>
+
+	<div
+		class="px-10 py-4 pb-2 flex flex-col items-center justify-between
+		text-white bg-pink-900 rounded-lg capitalize text-center">
+		<!-- Card -->
+
+		<div>
+			<h2>page impressions</h2>
+			<h1 class="mt-2 text-4xl font-semibold">8.6k</h1>
+		</div>
+
+		<div class="flex flex-row items-center">
+
+			<svg
+				class="h-5 w-5 fill-current text-red-400 mr-3"
+				viewBox="0 0 24 24">
+				<path
+					d="M22 11L12 21 2 11h6V3h8v8h6m-10 7l5-5h-3V5h-4v8H7l5 5z"></path>
+			</svg>
+
+			<span>9%</span>
+
+		</div>
+
+	</div>
+
+</div>
