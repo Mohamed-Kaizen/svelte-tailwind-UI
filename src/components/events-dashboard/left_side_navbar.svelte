2bf63499fb8bfad6e996fc417b11dabe2fe94a5a
Mohamed Nesredin <m.n.kaizen@gmail.com>
2020-07-13 09:31:34 +0300

    add lef side navbar for events dashboard

---
+++ src/components/events-dashboard/left_side_navbar.svelte
@@ -0,0 +1,105 @@
+<nav
+	class="w-16 py-2 flex flex-col items-center bg-white text-gray-500
+	dark:text-gray-400">
+
+	<ul class="mt-10">
+		<!-- Links -->
+
+		<li class="mt-5 p-3 text-black bg-white shadow-md rounded-lg">
+			<a href="events-dashboard/" class=" ">
+				<svg class="fill-current h-5 w-5" viewBox="0 0 24 24">
+					<path
+						d="M21 16V4H3v12h18m0-14a2 2 0 012 2v12a2 2 0 01-2
+						2h-7v2h2v2H8v-2h2v-2H3a2 2 0 01-2-2V4c0-1.11.89-2
+						2-2h18M5 6h9v5H5V6m10 0h4v2h-4V6m4 3v5h-4V9h4M5
+						12h4v2H5v-2m5 0h4v2h-4v-2z"></path>
+				</svg>
+			</a>
+		</li>
+
+		<li
+			class="mt-5 p-3 hover:text-black hover:bg-white hover:shadow-md
+			rounded-lg">
+			<a href="users-dashboard/">
+				<svg class="fill-current h-5 w-5" viewBox="0 0 24 24">
+					<path
+						d="M10 4a4 4 0 014 4 4 4 0 01-4 4 4 4 0 01-4-4 4 4 0
+						014-4m0 10c4.42 0 8 1.79 8 4v2H2v-2c0-2.21 3.58-4
+						8-4m10-2V7h2v6h-2m0 4v-2h2v2h-2z"></path>
+				</svg>
+			</a>
+		</li>
+
+		<li
+			class="mt-5 p-3 hover:text-black hover:bg-white hover:shadow-md
+			rounded-lg">
+			<a href="meetup/">
+				<svg class="fill-current h-5 w-5" viewBox="0 0 24 24">
+					<path
+						d="M19 19H5V8h14m-3-7v2H8V1H6v2H5c-1.11 0-2 .89-2 2v14a2
+						2 0 002 2h14a2 2 0 002-2V5a2 2 0 00-2-2h-1V1m-1
+						11h-5v5h5v-5z"></path>
+				</svg>
+			</a>
+		</li>
+
+		<li
+			class="mt-5 p-3 hover:text-black hover:bg-white hover:shadow-md
+			rounded-lg">
+			<a href="expenses-dashboard/">
+				<svg class="fill-current h-5 w-5" viewBox="0 0 24 24">
+					<path
+						d="M18 2c-.9 0-2 1-2 2H8c0-1-1.1-2-2-2H2v9c0 1 1 2 2
+						2h2.2c.4 2 1.7 3.7 4.8 4v2.08C8 19.54 8 22 8
+						22h8s0-2.46-3-2.92V17c3.1-.3 4.4-2 4.8-4H20c1 0 2-1
+						2-2V2h-4M6 11H4V4h2v7m14 0h-2V4h2v7z"></path>
+				</svg>
+
+			</a>
+		</li>
+
+		<li
+			class="mt-5 p-3 hover:text-black hover:bg-white hover:shadow-md
+			rounded-lg">
+			<a href="social-media-dashboard/">
+				<svg class="fill-current h-5 w-5" viewBox="0 0 24 24">
+					<path d="M22 21H2V3h2v16h2v-9h4v9h2V6h4v13h2v-5h4v7z"></path>
+				</svg>
+			</a>
+		</li>
+
+		<li
+			class="mt-5 p-3 hover:text-black hover:bg-white hover:shadow-md
+			rounded-lg">
+
+			<a href="teacher-dashboard/">
+				<svg class="fill-current h-5 w-5" viewBox="0 0 24 24">
+					<path
+						d="M20 11H4V8h16m0 7h-7v-2h7m0 6h-7v-2h7m-9
+						2H4v-6h7m9.33-8.33L18.67 3 17 4.67 15.33 3l-1.66 1.67L12
+						3l-1.67 1.67L8.67 3 7 4.67 5.33 3 3.67 4.67 2 3v16a2 2 0
+						002 2h16a2 2 0 002-2V3l-1.67 1.67z"></path>
+				</svg>
+			</a>
+
+		</li>
+
+	</ul>
+
+	<div
+		class="mt-auto flex items-center p-3 hover:text-red-600 hover:bg-red-200
+		dark:text-gray-400 rounded-lg">
+		<!-- important action -->
+
+		<a href="/">
+			<svg class="fill-current h-5 w-5" viewBox="0 0 24 24">
+				<path
+					d="M16 17v-3H9v-4h7V7l5 5-5 5M14 2a2 2 0 012
+					2v2h-2V4H5v16h9v-2h2v2a2 2 0 01-2 2H5a2 2 0 01-2-2V4a2 2 0
+					012-2h9z"></path>
+			</svg>
+		</a>
+
+	</div>
+
+</nav>
