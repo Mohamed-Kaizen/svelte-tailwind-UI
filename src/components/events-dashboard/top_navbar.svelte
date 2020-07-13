e5aa78ea3681e9407444a682be3c15179de67b33
Mohamed Nesredin <m.n.kaizen@gmail.com>
2020-07-13 08:56:18 +0300

    add top navbar for events dashboard

---
+++ src/components/events-dashboard/top_navbar.svelte
@@ -0,0 +1,119 @@
+<nav
+	class="h-24 w-full px-4 flex-shrink-0 flex justify-between fixed z-10
+	border-b bg-white">
+
+	<ul class="flex items-center">
+		<!-- left side -->
+
+		<li class="h-10 w-10">
+
+			<a href="events-dashboard/">
+
+				<img
+					class="h-full w-full mx-auto select-none"
+					src="django.png/"
+					alt="logo" />
+
+			</a>
+
+		</li>
+
+		<li class="ml-24">
+
+			<div class="relative text-gray-600">
+
+				<span
+					class="absolute right-0 mr-5 mt-2 hover:text-purple-700
+					cursor-pointer">
+
+					<svg viewBox="0 0 512 512" class="h-5 w-5 fill-current">
+						<path
+							d="M505 442.7L405.3
+							343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7
+							44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208
+							208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7
+							17l99.7 99.7c9.4 9.4 24.6 9.4 33.9
+							0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7
+							0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128
+							57.2 128 128 0 70.7-57.2 128-128 128z"></path>
+					</svg>
+
+				</span>
+
+				<input
+					class="bg-gray-200 rounded-full text-sm placeholder-gray-600
+					px-5 py-2"
+					placeholder="Search for something" />
+
+			</div>
+
+		</li>
+
+	</ul>
+
+	<ul class="flex items-center">
+		<!-- right side -->
+
+		<li>
+
+			<button
+				class="py-3 px-6 flex items-center text-white bg-purple-700
+				rounded-full shadow-lg">
+
+				<span>Create new</span>
+
+				<svg class="h-5 w-5 ml-3 fill-current" viewBox="0 0 24 24">
+					<path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"></path>
+				</svg>
+
+			</button>
+
+		</li>
+
+		<li class="mx-8">
+
+			<a href="inbox/" class="relative">
+
+				<span href="inbox/">
+
+					<svg
+						class="h-5 w-5 hover:text-purple-700"
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="currentColor"
+						stroke-width="2"
+						stroke-linecap="round"
+						stroke-linejoin="round">
+						<path
+							d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path>
+						<path d="M13.73 21a2 2 0 0 1-3.46 0"></path>
+					</svg>
+
+				</span>
+
+				<div class="absolute w-2 h-2 left-0 mb-2 ml-3 bottom-0">
+
+					<span
+						class="px-2 py-1 bg-red-500 rounded-full text-white
+						text-xs">
+						4
+					</span>
+
+				</div>
+
+			</a>
+
+		</li>
+
+		<li>
+
+			<img
+				class="h-10 w-10 rounded-full object-cover"
+				src="enoshima.png/"
+				alt="enoshima profile" />
+
+		</li>
+
+	</ul>
+
+</nav>
