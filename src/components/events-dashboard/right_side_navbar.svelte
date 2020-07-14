57a3d40dc3eed028f9e64d078a9edf31b6f23fc6
Mohamed Nesredin <m.n.kaizen@gmail.com>
2020-07-14 11:01:09 +0300

    add right side navbar for events dashboard

---
+++ src/components/events-dashboard/right_side_navbar.svelte
@@ -0,0 +1,95 @@
+<aside
+	class="w-1/4 px-16 py-8 flex flex-col bg-white text-gray-600 overflow-y-auto">
+
+	<span class="uppercase font-medium">recent activity</span>
+
+	<div class="mt-4 flex">
+
+		<img
+			class="mr-4 h-10 w-10 rounded-full object-cover"
+			src="joker.jpg/"
+			alt="joker profile" />
+
+		<div>
+
+			<p class="leading-snug mb-3">
+				Tom Ross has bought 3 Standard tickets on your event.
+			</p>
+
+			<span>3 min ago</span>
+
+		</div>
+
+	</div>
+
+	<div class="mt-4 flex">
+
+		<img
+			class="mr-4 h-10 w-10 rounded-full object-cover"
+			src="final_fantasy_xiii_2_lightning-2560x1440.jpg/"
+			alt="lightning profile" />
+
+		<div>
+
+			<p class="leading-snug mb-3">
+				Tom Ross has bought 3 Standard tickets on your event.
+			</p>
+
+			<span>3 min ago</span>
+
+		</div>
+
+	</div>
+	<div class="mt-4 flex">
+
+		<img
+			class="mr-4 h-10 w-10 rounded-full object-cover"
+			src="conan.png/"
+			alt="conan profile" />
+
+		<div>
+
+			<p class="leading-snug mb-3">
+				Tom Ross has bought 3 Standard tickets on your event.
+			</p>
+
+			<span>3 min ago</span>
+
+		</div>
+
+	</div>
+
+	<div class="mt-4 flex">
+
+		<img
+			class="mr-4 h-10 w-10 rounded-full object-cover"
+			src="mickey_mouse.jpg/"
+			alt="mickey profile" />
+
+		<div>
+
+			<p class="leading-snug mb-3">
+				Tom Ross has bought 3 Standard tickets on your event.
+			</p>
+
+			<span>3 min ago</span>
+
+		</div>
+
+	</div>
+
+	<span class="mt-8 uppercase font-medium">tickets selling</span>
+	<img src="events_dashboard_graph.png" alt="graph" />
+
+	<div class="mt-8 flex items-center justify-between">
+
+		<div class="flex flex-col">
+			<span class="text-black font-semibold capitalize">12 tickets</span>
+			<span class="text-sm capitalize">sold today</span>
+		</div>
+
+		<button class="py-2 px-4 border rounded-full">Analytics</button>
+
+	</div>
+
+</aside>
