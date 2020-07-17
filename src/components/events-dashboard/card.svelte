b65490cd548dae73a6a0d10fecca50162bbb6f8a
Mohamed Nesredin <m.n.kaizen@gmail.com>
2020-07-17 15:26:29 +0300

    add card for events dashboard

---
+++ src/components/events-dashboard/card.svelte
@@ -0,0 +1,107 @@
+<div
+	class="mt-6 px-12 py-10 flex flex-col justify-between rounded-lg
+	bg-purple-500 text-white capitalize">
+	<!-- Upcoming events card -->
+
+	<div class="flex justify-between">
+		<!-- Top side -->
+
+		<div class="flex text-xl font-semibold">
+			<!-- Top Left side -->
+
+			<div class="mr-4 pr-4 flex flex-col border-r">
+
+				<span class="text-xs">APR</span>
+				<span>21</span>
+			</div>
+
+			<span>ultimate championship 2019</span>
+
+		</div>
+
+		<div class="flex text-gray-300">
+			<!-- Top Right side -->
+
+			<span class=" mr-16">games</span>
+
+			<div class="flex flex-col items-end">
+				<span>enter fee</span>
+				<span class="text-white">$34</span>
+			</div>
+
+		</div>
+
+	</div>
+
+	<div class="mt-10 flex justify-between text-gray-300">
+		<!-- Bottom side  -->
+
+		<div class="flex w-full justify-between text-sm">
+
+			<div class="flex flex-col">
+				<span>selling starts</span>
+				<span class="text-green-300">started</span>
+
+			</div>
+
+			<div class="ml-4 flex flex-col">
+				<span>selling ends</span>
+				<span class="text-white">in 5 days</span>
+
+			</div>
+
+			<div class="ml-4 flex flex-col">
+				<span>players</span>
+				<span class="text-white">12</span>
+
+			</div>
+
+			<div class="ml-4 flex flex-col">
+				<span>teams</span>
+				<span class="text-white">2</span>
+
+			</div>
+
+			<div class="ml-4 flex flex-col">
+				<span>tickets left</span>
+				<span class="text-white">112</span>
+
+			</div>
+
+		</div>
+
+		<button
+			class="ml-20 flex items-center py-1 px-8 border border-gray-300
+			text-white rounded-full text-sm">
+
+			<svg
+				class="h-5 w-5 fill-current mr-2"
+				enable-background="new 0 0 24 24"
+				viewBox="0 0 24 24"
+				fill="black"
+				width="18px"
+				height="18px">
+				<g>
+					<path d="M0,0h24v24H0V0z" fill="none"></path>
+					<path
+						d="M19.14,12.94c0.04-0.3,0.06-0.61,0.06-0.94c0-0.32-0.02-0.64-0.07-0.94l2.03-1.58c0.18-0.14,0.23-0.41,0.12-0.61
+						l-1.92-3.32c-0.12-0.22-0.37-0.29-0.59-0.22l-2.39,0.96c-0.5-0.38-1.03-0.7-1.62-0.94L14.4,2.81c-0.04-0.24-0.24-0.41-0.48-0.41
+						h-3.84c-0.24,0-0.43,0.17-0.47,0.41L9.25,5.35C8.66,5.59,8.12,5.92,7.63,6.29L5.24,5.33c-0.22-0.08-0.47,0-0.59,0.22L2.74,8.87
+						C2.62,9.08,2.66,9.34,2.86,9.48l2.03,1.58C4.84,11.36,4.8,11.69,4.8,12s0.02,0.64,0.07,0.94l-2.03,1.58
+						c-0.18,0.14-0.23,0.41-0.12,0.61l1.92,3.32c0.12,0.22,0.37,0.29,0.59,0.22l2.39-0.96c0.5,0.38,1.03,0.7,1.62,0.94l0.36,2.54
+						c0.05,0.24,0.24,0.41,0.48,0.41h3.84c0.24,0,0.44-0.17,0.47-0.41l0.36-2.54c0.59-0.24,1.13-0.56,1.62-0.94l2.39,0.96
+						c0.22,0.08,0.47,0,0.59-0.22l1.92-3.32c0.12-0.22,0.07-0.47-0.12-0.61L19.14,12.94z
+						M12,15.6c-1.98,0-3.6-1.62-3.6-3.6
+						s1.62-3.6,3.6-3.6s3.6,1.62,3.6,3.6S13.98,15.6,12,15.6z"></path>
+				</g>
+			</svg>
+
+			<span class="mr-1">edit</span>
+
+			<span>evetns</span>
+
+		</button>
+
+	</div>
+
+</div>
