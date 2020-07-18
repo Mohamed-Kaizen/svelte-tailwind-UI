f8896665bd971a4e7c9bba46c2e08c5f926c4b3d
Mohamed Nesredin <m.n.kaizen@gmail.com>
2020-07-18 15:52:23 +0300

    add bottom card list and map section

---
+++ src/components/blood-donate-dashboard/bottom_card_list.svelte
@@ -0,0 +1,131 @@
+<div class="mr-8 w-2/6 capitalize">
+
+	<div class="flex justify-between font-semibold text-gray-600 text-xl">
+		<span>recent donors</span>
+		<button>
+
+			<svg class="h-4 w-4 fill-current text-black" viewBox="0 0 192 512">
+				<path
+					d="M96 184c39.8 0 72 32.2 72 72s-32.2 72-72 72-72-32.2-72-72
+					32.2-72 72-72zM24 80c0 39.8 32.2 72 72 72s72-32.2 72-72S135.8
+					8 96 8 24 40.2 24 80zm0 352c0 39.8 32.2 72 72 72s72-32.2
+					72-72-32.2-72-72-72-72 32.2-72 72z"></path>
+			</svg>
+
+		</button>
+	</div>
+
+	<div class="mt-5">
+
+		<div
+			class="p-5 flex items-center justify-between bg-white capitalize
+			font-medium rounded-lg">
+			<!-- Card -->
+
+			<div class="flex items-center">
+
+				<img
+					class="mr-8 h-10 w-10 rounded-full object-cover"
+					src="https://static0.srcdn.com/wordpress/wp-content/uploads/2017/05/Geoffery-Rush-as-Hector-Barbossa-in-Pirates-of-the-Caribbean.jpg?q=50&fit=crop&w=740&h=370"
+					alt="barbossa profile" />
+
+				<div class="flex flex-col">
+
+					<span>barbossa</span>
+
+					<span class="mt-1 text-sm text-gray-500">17 may 2020</span>
+
+				</div>
+
+			</div>
+
+			<span class="p-3 rounded-lg bg-pink-200 text-red-700">O+</span>
+
+		</div>
+
+		<div
+			class="mt-2 p-5 flex items-center justify-between bg-white capitalize
+			font-medium rounded-lg">
+			<!-- Card -->
+
+			<div class="flex items-center">
+
+				<img
+					class="mr-8 h-10 w-10 rounded-full object-cover"
+					src="https://data.whicdn.com/images/341568668/original.jpg"
+					alt="carla profile" />
+
+				<div class="flex flex-col">
+
+					<span>carla</span>
+
+					<span class="mt-1 text-sm text-gray-500">17 may 2020</span>
+
+				</div>
+
+			</div>
+
+			<span class="p-3 rounded-lg bg-pink-200 text-red-700">B+</span>
+
+		</div>
+
+		<div
+			class="mt-2 p-5 flex items-center justify-between bg-white capitalize
+			font-medium rounded-lg">
+			<!-- Card -->
+
+			<div class="flex items-center">
+
+				<img
+					class="mr-8 h-10 w-10 rounded-full object-cover"
+					src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/0909-mila-kunis-1445035501.jpg"
+					alt="mila kunis profile" />
+
+				<div class="flex flex-col">
+
+					<span>mila kunis</span>
+
+					<span class="mt-1 text-sm text-gray-500">17 may 2020</span>
+
+				</div>
+
+			</div>
+
+			<span class="p-3 rounded-lg bg-pink-200 text-red-700">A+</span>
+
+		</div>
+
+		<div
+			class="mt-2 p-5 flex items-center justify-between bg-white capitalize
+			font-medium rounded-lg">
+			<!-- Card -->
+
+			<div class="flex items-center">
+
+				<img
+					class="mr-8 h-10 w-10 rounded-full object-cover"
+					src="https://vignette.wikia.nocookie.net/christmasspecials/images/3/3a/Bugs_in_Bah_Humduck.jpg/revision/latest?cb=20181226001516"
+					alt="bugs bunny profile" />
+
+				<div class="flex flex-col">
+
+					<span>bugs bunny</span>
+
+					<span class="mt-1 text-sm text-gray-500">17 may 2020</span>
+
+				</div>
+
+			</div>
+
+			<span class="p-3 rounded-lg bg-pink-200 text-red-700">AB-</span>
+
+		</div>
+		<div
+			class="py-6 flex items-center justify-center bg-gray-300 rounded-b-lg
+			text-blue-500 capitalize">
+			<a href="/">see more</a>
+		</div>
+
+	</div>
+
+</div>
---
+++ src/components/blood-donate-dashboard/map_section.svelte
@@ -0,0 +1,37 @@
+<div class="ml-8 w-3/4">
+
+	<div
+		class="flex justify-between font-semibold text-gray-600 text-xl
+		capitalize">
+
+		<span>track nearby donors</span>
+
+		<div>
+
+			<select class="mr-6 bg-white rounded-lg">
+				<option>O+</option>
+				<option>O</option>
+				<option>B-</option>
+				<option>A+</option>
+				<option>AB-</option>
+			</select>
+
+			<button>
+
+				<svg
+					class="h-4 w-4 fill-current text-black"
+					viewBox="0 0 192 512">
+					<path
+						d="M96 184c39.8 0 72 32.2 72 72s-32.2 72-72
+						72-72-32.2-72-72 32.2-72 72-72zM24 80c0 39.8 32.2 72 72
+						72s72-32.2 72-72S135.8 8 96 8 24 40.2 24 80zm0 352c0 39.8
+						32.2 72 72 72s72-32.2 72-72-32.2-72-72-72-72 32.2-72 72z"></path>
+				</svg>
+
+			</button>
+
+		</div>
+
+	</div>
+
+</div>
