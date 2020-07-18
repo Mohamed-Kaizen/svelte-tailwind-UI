3dcbbf1a7bb30a32df04e8cf8bad45b06c83dbce
Mohamed Nesredin <m.n.kaizen@gmail.com>
2020-07-18 10:13:38 +0300

    add side navbar for blood donate dashboard

---
+++ src/components/blood-donate-dashboard/side_navbar.svelte
@@ -0,0 +1,202 @@
+<nav class="w-64 px-16 py-10 flex flex-col items-center overflow-y-auto">
+
+	<div>
+		<!-- App Logo -->
+
+		<img src="/download-removebg-preview.png" alt="logo" />
+
+	</div>
+
+	<ul class="mt-8">
+		<!-- Links -->
+
+		<li class="px-5 py-3 rounded-lg bg-gray-200 text-gray-700 ">
+
+			<a
+				href="#home"
+				class="pr-4 -mr-5 flex items-center border-r-2 border-red-600">
+
+				<svg
+					class="h-5 w-5 fill-current text-red-600"
+					viewBox="0 0 24 24">
+					<path d="M0 0h24v24H0z" fill="none"></path>
+					<path
+						d="M3 13h8V3H3v10zm0 8h8v-6H3v6zm10
+						0h8V11h-8v10zm0-18v6h8V3h-8z"></path>
+				</svg>
+
+				<span class="ml-2 capitalize font-medium">dashboard</span>
+
+			</a>
+
+		</li>
+
+		<li
+			class="mt-8 px-5 py-3 rounded-lg text-gray-500 hover:bg-gray-200
+			hover:text-red-600">
+
+			<a
+				href="#home"
+				class="pr-4 -mr-5 flex items-center hover:border-r-2
+				border-red-600">
+				<svg
+					class="h-5 w-5 fill-current"
+					viewBox="0 0 24 24"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round">
+					<path
+						d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12
+						5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12
+						21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
+				</svg>
+
+				<span class="ml-2 capitalize font-medium">donors</span>
+
+			</a>
+
+		</li>
+
+		<li
+			class="mt-8 px-5 py-3 rounded-lg text-gray-500 hover:bg-gray-200
+			hover:text-red-600">
+
+			<a
+				href="#home"
+				class="pr-4 -mr-5 flex items-center hover:border-r-2
+				border-red-600">
+				<svg
+					aria-hidden="true"
+					focusable="false"
+					data-prefix="fas"
+					data-icon="hand-holding-water"
+					role="img"
+					class="h-5 w-5 fill-current"
+					viewBox="0 0 576 512">
+					<path
+						fill="currentColor"
+						d="M288 256c53 0 96-42.1 96-94
+						0-40-57.1-120.7-83.2-155.6-6.4-8.5-19.2-8.5-25.6 0C249.1
+						41.3 192 122 192 162c0 51.9 43 94 96 94zm277.3
+						72.1c-11.8-10.7-30.2-10-42.6 0L430.3 402c-11.3 9.1-25.4
+						14-40 14H272c-8.8 0-16-7.2-16-16s7.2-16 16-16h78.3c15.9 0
+						30.7-10.9 33.3-26.6 3.3-20-12.1-37.4-31.6-37.4H192c-27
+						0-53.1 9.3-74.1 26.3L71.4 384H16c-8.8 0-16 7.2-16 16v96c0
+						8.8 7.2 16 16 16h356.8c14.5 0 28.6-4.9 40-14L564
+						377c15.2-12.1 16.4-35.3 1.3-48.9z"></path>
+				</svg>
+
+				<span class="ml-2 capitalize font-medium">receiver</span>
+
+			</a>
+
+		</li>
+
+		<li
+			class="mt-8 px-5 py-3 rounded-lg text-gray-500 hover:bg-gray-200
+			hover:text-red-600">
+
+			<a
+				href="#home"
+				class="pr-4 -mr-5 flex items-center hover:border-r-2
+				border-red-600">
+				<svg
+					aria-hidden="true"
+					focusable="false"
+					data-prefix="fas"
+					data-icon="hospital-alt"
+					class="h-5 w-5 fill-current"
+					role="img"
+					viewBox="0 0 576 512">
+					<path
+						fill="currentColor"
+						d="M544 96H416V32c0-17.7-14.3-32-32-32H192c-17.7 0-32
+						14.3-32 32v64H32c-17.7 0-32 14.3-32 32v368c0 8.8 7.2 16
+						16 16h544c8.8 0 16-7.2
+						16-16V128c0-17.7-14.3-32-32-32zM160 436c0 6.6-5.4 12-12
+						12h-40c-6.6 0-12-5.4-12-12v-40c0-6.6 5.4-12 12-12h40c6.6
+						0 12 5.4 12 12v40zm0-128c0 6.6-5.4 12-12 12h-40c-6.6
+						0-12-5.4-12-12v-40c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12
+						12v40zm160 128c0 6.6-5.4 12-12 12h-40c-6.6
+						0-12-5.4-12-12v-40c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12
+						12v40zm0-128c0 6.6-5.4 12-12 12h-40c-6.6
+						0-12-5.4-12-12v-40c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12
+						12v40zm16-170c0 3.3-2.7 6-6 6h-26v26c0 3.3-2.7 6-6
+						6h-20c-3.3 0-6-2.7-6-6v-26h-26c-3.3 0-6-2.7-6-6v-20c0-3.3
+						2.7-6 6-6h26V86c0-3.3 2.7-6 6-6h20c3.3 0 6 2.7 6
+						6v26h26c3.3 0 6 2.7 6 6v20zm144 298c0 6.6-5.4 12-12
+						12h-40c-6.6 0-12-5.4-12-12v-40c0-6.6 5.4-12 12-12h40c6.6
+						0 12 5.4 12 12v40zm0-128c0 6.6-5.4 12-12 12h-40c-6.6
+						0-12-5.4-12-12v-40c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12
+						12v40z"></path>
+				</svg>
+
+				<span class="ml-2 capitalize font-medium">hospitals</span>
+
+			</a>
+
+		</li>
+
+		<li
+			class="mt-8 px-5 py-3 rounded-lg text-gray-500 hover:bg-gray-200
+			hover:text-red-600">
+
+			<a
+				href="#home"
+				class="pr-4 -mr-5 flex items-center hover:border-r-2
+				border-red-600">
+				<svg class="h-5 w-5 fill-current" viewBox="0 0 24 24">
+					<path d="M0 0h24v24H0z" fill="none"></path>
+					<path
+						d="M20 3h-1V1h-2v2H7V1H5v2H4c-1.1 0-2 .9-2 2v16c0 1.1.9 2
+						2 2h16c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 18H4V8h16v13z"></path>
+				</svg>
+				<span class="ml-2 capitalize font-medium">calender</span>
+
+			</a>
+
+		</li>
+
+		<li
+			class="mt-8 px-5 py-3 rounded-lg text-gray-500 hover:bg-gray-200
+			hover:text-red-600">
+
+			<a
+				href="#home"
+				class="pr-4 -mr-5 flex items-center hover:border-r-2
+				border-red-600">
+				<svg
+					class="h-5 w-5 "
+					viewBox="0 0 24 24"
+					fill="none"
+					stroke="currentColor"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round">
+					<circle cx="12" cy="12" r="3"></circle>
+					<path
+						d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0
+						2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33
+						1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0
+						1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0
+						0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1
+						0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0
+						0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0
+						0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1
+						0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0
+						1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0
+						1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0
+						1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0
+						2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0
+						1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0
+						0-1.51 1z"></path>
+				</svg>
+
+				<span class="ml-2 capitalize font-medium">settings</span>
+
+			</a>
+
+		</li>
+
+	</ul>
+</nav>
