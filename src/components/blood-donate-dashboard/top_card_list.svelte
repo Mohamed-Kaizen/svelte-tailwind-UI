36451d4d5a18f2b95470edaa21ff6d258f7cad17
Mohamed Nesredin <m.n.kaizen@gmail.com>
2020-07-18 14:29:37 +0300

    add top card list for blood donate dashboard

---
+++ src/components/blood-donate-dashboard/top_card_list.svelte
@@ -0,0 +1,79 @@
+<div class="mt-12 flex justify-between">
+	<!-- List of cards -->
+
+	<div
+		class="h-40 w-1/3 p-5 flex flex-col justify-between bg-blue-200
+		text-blue-700 font-semibold capitalize rounded-lg">
+		<!-- Card -->
+
+		<div class="flex items-center justify-between">
+			<!-- Top -->
+
+			<span class="text-2xl">requested</span>
+			<span class="text-xs">18 may 2020</span>
+
+		</div>
+
+		<div class="flex justify-between">
+			<!-- Bottom -->
+
+			<div class="flex flex-col">
+				<span class="text-xs">total</span>
+				<span class="text-2xl">20</span>
+			</div>
+
+		</div>
+
+	</div>
+
+	<div
+		class="h-40 w-1/3 ml-8 p-5 flex flex-col justify-between bg-pink-200
+		text-red-700 font-semibold capitalize rounded-lg">
+		<!-- Card -->
+
+		<div class="flex items-center justify-between">
+			<!-- Top -->
+
+			<span class="text-2xl">received</span>
+			<span class="text-xs">18 may 2020</span>
+
+		</div>
+
+		<div class="flex justify-between">
+			<!-- Bottom -->
+
+			<div class="flex flex-col">
+				<span class="text-xs">total</span>
+				<span class="text-2xl">12</span>
+			</div>
+
+		</div>
+
+	</div>
+
+	<div
+		class="h-40 w-1/3 ml-8 p-5 flex flex-col justify-between bg-green-200
+		text-green-700 font-semibold capitalize rounded-lg">
+		<!-- Card -->
+
+		<div class="flex items-center justify-between">
+			<!-- Top -->
+
+			<span class="text-2xl">in stock</span>
+			<span class="text-xs">18 may 2020</span>
+
+		</div>
+
+		<div class="flex justify-between">
+			<!-- Bottom -->
+
+			<div class="flex flex-col">
+				<span class="text-xs">total</span>
+				<span class="text-2xl">130</span>
+			</div>
+
+		</div>
+
+	</div>
+
+</div>
