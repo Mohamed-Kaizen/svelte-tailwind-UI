e0c35c4f7aeb49329ccabfdc8259e0b554c532f6
Mohamed Nesredin <m.n.kaizen@gmail.com>
2020-07-18 13:53:18 +0300

    add header for blood donate dashboard

---
+++ src/components/blood-donate-dashboard/header.svelte
@@ -0,0 +1,47 @@
+<div class="flex items-center justify-end">
+
+	<a href="inbox/" class="relative mr-12">
+
+		<span>
+			<svg
+				class="h-5 w-5 hover:text-red-600 dark-hover:text-red-400"
+				viewBox="0 0 24 24"
+				fill="none"
+				stroke="currentColor"
+				stroke-width="2"
+				stroke-linecap="round"
+				stroke-linejoin="round">
+				<path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path>
+				<path d="M13.73 21a2 2 0 0 1-3.46 0"></path>
+			</svg>
+		</span>
+
+		<div
+			class="absolute w-2 h-2 rounded-full bg-red-600 right-0 mb-4 bottom-0"></div>
+
+	</a>
+
+	<span class="mr-4 flex flex-col items-end capitalize">
+		<span class="font-semibold ">johns hopkins hospital</span>
+		<span class="text-gray-600">massachusetts</span>
+
+	</span>
+
+	<img
+		class="mr-4 h-10 w-10 rounded-lg object-cover"
+		src="https://www.syfy.com/sites/syfy/files/styles/1200x680_hero/public/2018/12/screen_shot_2018-12-21_at_11.08.39_am.png"
+		alt=" jack sparrow profile" />
+
+	<button>
+
+		<svg class="h-4 w-4 fill-current" viewBox="0 0 192 512">
+			<path
+				d="M96 184c39.8 0 72 32.2 72 72s-32.2 72-72 72-72-32.2-72-72
+				32.2-72 72-72zM24 80c0 39.8 32.2 72 72 72s72-32.2 72-72S135.8 8
+				96 8 24 40.2 24 80zm0 352c0 39.8 32.2 72 72 72s72-32.2
+				72-72-32.2-72-72-72-72 32.2-72 72z"></path>
+		</svg>
+
+	</button>
+
+</div>
