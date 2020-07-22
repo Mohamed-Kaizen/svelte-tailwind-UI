768de98bd9e9a3fb2df5af1c50f0db47dcef5e00
Mohamed Nesredin <m.n.kaizen@gmail.com>
2020-07-22 16:14:20 +0300

    add top navbar for jobs list

---
+++ src/components/jobs-list/top_navbar.svelte
@@ -0,0 +1,113 @@
+<nav
+	class=" h-20 w-full px-20 py-8 flex items-center justify-between border-b
+	bg-white">
+
+	<ul class="flex items-center">
+		<!-- left side -->
+
+		<li class="h-10 w-10">
+
+			<a href="http://svelte.dev/" target="blank">
+
+				<img
+					class="h-full w-full mx-auto select-none"
+					src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Svelte_Logo.svg/1200px-Svelte_Logo.svg.png"
+					alt="logo" />
+
+			</a>
+
+		</li>
+
+		<li class="ml-8">
+
+			<a
+				href="jobs-list/"
+				class="block border-b-4 border-black focus:outline-none
+				font-medium capitalize">
+				jobs
+			</a>
+
+		</li>
+
+		<li class="ml-6">
+
+			<button
+				class="block border-b-4 border-transparent focus:outline-none
+				font-medium capitalize text-center focus:border-black transition
+				duration-300 ease-in-out">
+				companies
+			</button>
+
+		</li>
+
+		<li class="ml-6">
+
+			<button
+				class="block border-b-4 border-transparent focus:outline-none
+				font-medium capitalize text-center focus:border-black transition
+				duration-300 ease-in-out">
+				network
+			</button>
+
+		</li>
+
+	</ul>
+
+	<ul class="flex items-center">
+		<!-- right side -->
+
+		<li class="mr-6">
+
+			<a
+				href="blood-donate-dashboard/"
+				class="block focus:outline-none focus:text-blue-500 font-medium
+				capitalize">
+				saved (7)
+			</a>
+
+		</li>
+
+		<li class="mr-6 relative">
+
+			<a
+				href="inbox/"
+				class="block focus:outline-none focus:text-blue-500 font-medium
+				capitalize">
+				messages (5)
+			</a>
+			<div
+				class="absolute w-2 h-2 rounded-full bg-blue-600 right-0 -mr-1
+				mb-4 bottom-0"></div>
+
+		</li>
+
+		<li class="mr-6">
+
+			<a
+				href="events-dashboard/"
+				class="block focus:outline-none focus:text-blue-500 font-medium
+				capitalize">
+				help
+			</a>
+
+		</li>
+
+		<li class="h-10 w-10">
+
+			<a
+				href="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/0909-mila-kunis-1445035501.jpg"
+				target="blank">
+
+				<img
+					class="h-full w-full mx-auto select-none rounded-full
+					object-cover"
+					src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/0909-mila-kunis-1445035501.jpg"
+					alt="mila kunis profile" />
+
+			</a>
+
+		</li>
+
+	</ul>
+
+</nav>
