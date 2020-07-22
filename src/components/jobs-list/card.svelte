27454c2cb1d116d7a98ac0b61eefc7c5fdcadc4e
Mohamed Nesredin <m.n.kaizen@gmail.com>
2020-07-22 20:19:57 +0300

    add card and list_header for jobs list

---
+++ src/components/jobs-list/card.svelte
@@ -0,0 +1,58 @@
+<div class="mt-6 flex items-center justify-between border-b pb-5">
+	<!-- Card -->
+
+	<div class="flex">
+
+		<img
+			class="h-10 w-10 rounded-full object-cover"
+			src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/1024px-Spotify_logo_without_text.svg.png"
+			alt="spotify logo" />
+
+		<div class="ml-5 flex flex-col leading-snug">
+			<p class="w-56 font-semibold">
+				senior product designer - search experience
+			</p>
+			<p class="text-sm text-gray-600">spotify &middot; 1 day ago</p>
+		</div>
+
+	</div>
+
+	<div class="flex flex-col leading-snug">
+		<span class="font-medium">full-time</span>
+		<span class="text-sm text-gray-600">stockolm, swedan</span>
+	</div>
+	<div class="flex">
+
+		<button class="mr-10">
+			<svg
+				class="h-6 w-6 fill-current"
+				viewBox="0 0 24 24"
+				fill="black"
+				width="18px"
+				height="18px">
+				<path d="M0 0h24v24H0V0z" fill="none"></path>
+				<path
+					d="M17 3H7c-1.1 0-2 .9-2 2v16l7-3 7 3V5c0-1.1-.9-2-2-2zm0
+					15l-5-2.18L7 18V5h10v13z"></path>
+			</svg>
+		</button>
+
+		<button>
+			<svg
+				class="h-6 w-6 fill-current"
+				viewBox="0 0 24 24"
+				fill="black"
+				width="18px"
+				height="18px">
+				<path d="M0 0h24v24H0V0z" fill="none"></path>
+				<path
+					d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1
+					0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9
+					2 2 2 2-.9 2-2-.9-2-2-2z"></path>
+			</svg>
+
+		</button>
+
+	</div>
+
+</div>
---
+++ src/components/jobs-list/list_header.svelte
@@ -0,0 +1,32 @@
+<div class="flex items-center justify-between">
+	<!-- Header -->
+
+	<span>
+		sort by
+		<strong>most recent</strong>
+	</span>
+
+	<div class="flex">
+
+		<button class="text-gray-500 focus:text-black">
+			<svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
+				<path d="M0 0h24v24H0V0z" fill="none"></path>
+				<path
+					d="M4 5v13h17V5H4zm10 2v3.5h-3V7h3zM6 7h3v3.5H6V7zm0
+					9v-3.5h3V16H6zm5 0v-3.5h3V16h-3zm8
+					0h-3v-3.5h3V16zm-3-5.5V7h3v3.5h-3z"></path>
+			</svg>
+		</button>
+
+		<button>
+			<svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
+				<path d="M0 0h24v24H0V0z" fill="none" opacity=".87"></path>
+				<path
+					d="M3 5v14h17V5H3zm4 2v2H5V7h2zm-2 6v-2h2v2H5zm0
+					2h2v2H5v-2zm13 2H9v-2h9v2zm0-4H9v-2h9v2zm0-4H9V7h9v2z"></path>
+			</svg>
+		</button>
+
+	</div>
+
+</div>
