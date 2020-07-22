71478229bec42c16b4a10407f1d4d4452230c933
Mohamed Nesredin <m.n.kaizen@gmail.com>
2020-07-22 14:02:52 +0300

    update deps

--- src/server.js
+++ src/server.js
@@ -6,9 +6,8 @@ import * as sapper from "@sapper/server"
 const {PORT, NODE_ENV} = process.env
 const dev = NODE_ENV === "development"
 
-const app = polka() // You can also use Express
+export const app = polka() // You can also use Express
 	.use(compression({threshold: 0}), sirv("static", {dev}), sapper.middleware())
 	.listen(PORT, (err) => {
 		if (err) console.log("error", err)
 	})
-export default app.handler
