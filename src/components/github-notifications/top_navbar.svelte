7773ead9932c653f924ef1273493ade79257e0e8
Mohamed Nesredin <m.n.kaizen@gmail.com>
2020-07-23 14:30:18 +0300

    add top navbar for github notifications

---
+++ src/components/github-notifications/top_navbar.svelte
@@ -0,0 +1,168 @@
+<nav
+	class="w-full h-16 lg:h-12 p-4 flex items-center justify-between bg-gray-900
+	text-white">
+
+	<button class="lg:hidden hover:text-gray-500">
+		<!-- Left side -->
+
+		<svg class="h-6 w-6 fill-current" viewBox="0 0 16 16" aria-hidden="true">
+			<path
+				d="M1 2.75A.75.75 0 011.75 2h12.5a.75.75 0 110 1.5H1.75A.75.75 0
+				011 2.75zm0 5A.75.75 0 011.75 7h12.5a.75.75 0 110 1.5H1.75A.75.75
+				0 011 7.75zM1.75 12a.75.75 0 100 1.5h12.5a.75.75 0 100-1.5H1.75z"></path>
+		</svg>
+
+	</button>
+
+	<div class="flex items-center text-sm font-semibold">
+		<!-- Middle side -->
+
+		<a href="https://github.com/" target="blank">
+
+			<svg
+				class="h-8 w-8 fill-current"
+				viewBox="0 0 16 16"
+				aria-hidden="true">
+				<path
+					d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47
+					7.59.4.07.55-.17.55-.38
+					0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01
+					1.08.58 1.23.82.72 1.21 1.87.87
+					2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95
+					0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21
+					2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04
+					2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15
+					0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01
+					1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016
+					8c0-4.42-3.58-8-8-8z"></path>
+			</svg>
+
+		</a>
+
+		<span class="hidden lg:block ml-4 relative text-sm">
+
+			<input
+				class="w-full px-3 py-1 bg-gray-800 placeholder-gray-300
+				rounded-lg focus:outline-none focus:bg-white focus:text-gray-600
+				transition duration-300 ease-in-out"
+				placeholder="Search or jump to..." />
+
+			<button
+				class="mt-1 mr-1 absolute right-0 text-center capitalize
+				text-white rounded-lg">
+
+				<svg class="h-5 w-5" viewBox="0 0 19 20">
+					<path
+						fill="none"
+						stroke="#979A9C"
+						opacity="0.4"
+						d="M3.5,0.5h12c1.7,0,3,1.3,3,3v13c0,1.7-1.3,3-3,3h-12c-1.7,0-3-1.3-3-3v-13C0.5,1.8,1.8,0.5,3.5,0.5z"></path>
+					<path
+						fill="#979A9C"
+						d="M11.8,6L8,15.1H7.1L10.8,6L11.8,6z"></path>
+				</svg>
+
+			</button>
+
+		</span>
+
+		<a
+			class="hidden lg:block ml-4 hover:text-gray-500 capitalize"
+			href="https://github.com/pulls"
+			target="blank">
+			pull requests
+		</a>
+
+		<a
+			class="hidden lg:block ml-4 hover:text-gray-500 capitalize"
+			href="https://github.com/pulls"
+			target="blank">
+			issues
+		</a>
+
+		<a
+			class="hidden lg:block ml-4 hover:text-gray-500 capitalize"
+			href="https://github.com/pulls"
+			target="blank">
+			marketplace
+		</a>
+
+		<a
+			class="hidden lg:block ml-4 hover:text-gray-500 capitalize"
+			href="https://github.com/pulls"
+			target="blank">
+			explore
+		</a>
+
+	</div>
+
+	<div class="flex items-center">
+		<!-- Right side -->
+
+		<div class="mr-4 relative">
+
+			<a
+				class="hover:text-gray-500"
+				href="https://github.com/notifications"
+				target="blank">
+
+				<svg
+					class="h-4 w-4 fill-current"
+					viewBox="0 0 16 16"
+					aria-hidden="true">
+					<path
+						d="M8 16a2 2 0
+						001.985-1.75c.017-.137-.097-.25-.235-.25h-3.5c-.138
+						0-.252.113-.235.25A2 2 0 008 16z"></path>
+					<path
+						d="M8 1.5A3.5 3.5 0 004.5 5v2.947c0
+						.346-.102.683-.294.97l-1.703 2.556a.018.018 0
+						00-.003.01l.001.006c0 .002.002.004.004.006a.017.017 0
+						00.006.004l.007.001h10.964l.007-.001a.016.016 0
+						00.006-.004.016.016 0 00.004-.006l.001-.007a.017.017 0
+						00-.003-.01l-1.703-2.554a1.75 1.75 0 01-.294-.97V5A3.5
+						3.5 0 008 1.5zM3 5a5 5 0 0110 0v2.947c0
+						.05.015.098.042.139l1.703 2.555A1.518 1.518 0 0113.482
+						13H2.518a1.518 1.518 0 01-1.263-2.36l1.703-2.554A.25.25 0
+						003 7.947V5z"></path>
+				</svg>
+
+			</a>
+
+			<span
+				class="absolute w-2 h-2 rounded-full bg-blue-500 left-0 ml-2
+				-mt-1 top-0 "></span>
+
+		</div>
+
+		<button class="hidden lg:flex mr-4 items-center hover:text-gray-500">
+
+			<svg class="h-5 w-5 fill-current" viewBox="0 0 24 24">
+				<path d="M0 0h24v24H0V0z" fill="none"></path>
+				<path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"></path>
+			</svg>
+
+			<svg class="h-5 w-5 fill-current" viewBox="0 0 24 24">
+				<path d="M0 0h24v24H0V0z" fill="none"></path>
+				<path d="M7 10l5 5 5-5H7z"></path>
+			</svg>
+
+		</button>
+
+		<button class="hidden lg:flex items-center hover:text-gray-500">
+
+			<img
+				class="h-5 w-5 rounded-full"
+				src="https://avatars1.githubusercontent.com/u/35686076?s=60&v=4"
+				alt="profile" />
+
+			<svg class="h-5 w-5 fill-current" viewBox="0 0 24 24">
+				<path d="M0 0h24v24H0V0z" fill="none"></path>
+				<path d="M7 10l5 5 5-5H7z"></path>
+
+			</svg>
+
+		</button>
+
+	</div>
+</nav>
