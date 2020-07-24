29485dd95a29ee4cd0cabbc3fdb7337dbd69dd4e
Mohamed Nesredin <m.n.kaizen@gmail.com>
2020-07-24 06:59:13 +0300

    update home page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,8 @@
 <script>
 	const projects = [
+		["Meetup", "meetup.png", "meetup/"],
+		["Inbox", "inbox.png", "inbox/"],
+		["Issue Dashboard", "issue_dashboard.png", "issues-dashboard/"],
 		["Expenses Dashboard", "expense.png", "expenses-dashboard/"],
 		["User Dashboard", "user-dashboard.png", "users-dashboard/"],
 		[
@@ -26,34 +29,33 @@
 	class="container mx-auto p-8 flex flex-row flex-wrap justify-between
 	capitalize">
 
-	<div class="m-8 shadow px-6 py-2 text-blue-500 bg-white">
-		<a href="/meetup/">Meetup</a>
-
-	</div>
-
-	<div class="m-8 shadow px-6 py-2 bg-white">
-		<a class="text-blue-500" href="/inbox/">Inbox</a>
-
-	</div>
-
-	<div class="m-8 shadow px-6 py-2 bg-white">
-		<a class="text-blue-500" href="issues-dashboard/">issues Dashboard</a>
-
-	</div>
-
 	{#each projects as [name, image, link]}
 		<div
-			class="m-8 shadow px-6 py-2 flex items-center text-blue-500 bg-white">
-
-			<a href="{image}/" target="blank">
-				<img
-					class="h-10 w-10 mr-3 rounded-full"
-					src="{image}/"
-					alt="{name}" />
-			</a>
-
-			<a href="{link}">{name}</a>
-
+			class="flex flex-col justify-center items-center max-w-sm mx-auto
+			my-8">
+			<img
+				src="{image}/"
+				class="bg-gray-300 h-64 w-full rounded-lg shadow-md bg-cover
+				bg-center"
+				alt="{name}" />
+			<div
+				class="w-56 md:w-64 bg-white -mt-10 shadow-lg rounded-lg
+				overflow-hidden">
+				<div
+					class="py-2 text-center font-bold uppercase tracking-wide
+					text-gray-800">
+					{name}
+				</div>
+				<div
+					class="flex items-center justify-center py-2 px-3 bg-gray-400">
+					<a
+						href="{link}"
+						class=" bg-gray-800 text-xs text-white px-2 py-1
+						font-semibold rounded uppercase hover:bg-gray-700">
+						check it out
+					</a>
+				</div>
+			</div>
 		</div>
 	{/each}
 
