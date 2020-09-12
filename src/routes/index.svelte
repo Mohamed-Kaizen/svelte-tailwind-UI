e0530a830490f216c2ce11c40f06aac3377b48db
Mohamed Nesredin <m.n.kaizen@gmail.com>
2020-09-12 13:53:40 +0300

    add Github Notifications page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -20,6 +20,11 @@
 			"blood-donate-dashboard/",
 		],
 		["Jobs List", "job_list.png", "jobs-list/"],
+		[
+			"Github Notifications",
+			"github_notifications.png",
+			"github-notifications/",
+		],
 	]
 </script>
 
@@ -31,36 +36,34 @@
 
 <div
 	class="container mx-auto p-8 flex flex-row flex-wrap justify-between
-	capitalize">
-
+		capitalize">
 	{#each projects as [name, image, link]}
 		<div
 			class="flex flex-col justify-center items-center max-w-sm mx-auto
-			my-8">
+				my-8">
 			<img
 				src="{image}/"
 				class="bg-gray-300 h-64 w-full rounded-lg shadow-md bg-cover
-				bg-center"
+					bg-center"
 				alt="{name}" />
 			<div
 				class="w-56 md:w-64 bg-white -mt-10 shadow-lg rounded-lg
-				overflow-hidden">
+					overflow-hidden">
 				<div
 					class="py-2 text-center font-bold uppercase tracking-wide
-					text-gray-800">
+						text-gray-800">
 					{name}
 				</div>
 				<div
 					class="flex items-center justify-center py-2 px-3 bg-gray-400">
 					<a
 						href="{link}"
-						class=" bg-gray-800 text-xs text-white px-2 py-1
-						font-semibold rounded uppercase hover:bg-gray-700">
+						class="bg-gray-800 text-xs text-white px-2 py-1
+							font-semibold rounded uppercase hover:bg-gray-700">
 						check it out
 					</a>
 				</div>
 			</div>
 		</div>
 	{/each}
-
 </div>
